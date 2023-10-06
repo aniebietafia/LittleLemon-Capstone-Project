aeccfcdb139ef04cb81190aa5ed3a824527e8ecf
aniebietafia <aniebietafia87@gmail.com>
2023-10-06 19:32:19 +0100

    app views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,21 @@
+from rest_framework.decorators import api_view
 from django.shortcuts import render
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
