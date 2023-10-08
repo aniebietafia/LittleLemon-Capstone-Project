f378d7c52c75923bb6647ae167577700f91da4c6
aniebietafia <aniebietafia87@gmail.com>
2023-10-08 17:42:48 +0100

    adding test cases

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,9 +6,12 @@ from django.db import models
 class Menu(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     no_of_guests = models.IntegerField()
     booking_date = models.DateField()
-    # image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}: {str(self.price)}"
 
 
 class Booking(models.Model):
